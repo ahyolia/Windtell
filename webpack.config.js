09adcf3e68b415af1a0c6021188e15a157e799a4
Amin Handoyo Camélia <camhdyo@gmail.com>
2025-09-30 15:42:42 +1100

    déploiement debug

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,13 @@ module.exports = async function (env, argv) {
     delete config.devServer._assetEmittingPreviousFiles;
   }
 
+  config.resolve = config.resolve || {};
+  config.resolve.fallback = {
+    ...(config.resolve.fallback || {}),
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+    vm: false, 
+  };
+
   return config;
 };
